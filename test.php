17a3d2090d401d89910836a231855d448e5b96e4
darkvier <javi_wallace@hotmail.com>
2012-12-20 15:19:42 +0100

    Albums (inacabado) Funcion limpieza caracteres raros Estilo de boton para todos Reorganizado el sistema de fotos (inacabado)

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+function sanear_string($string) {
+	$string = trim($string);
+	$string = str_replace(array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'), array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'), $string);
+	$string = str_replace(array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'), array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'), $string);
+	$string = str_replace(array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'), array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'), $string);
+	$string = str_replace(array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'), array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'), $string);
+	$string = str_replace(array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'), array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'), $string);
+	$string = str_replace(array('ñ', 'Ñ', 'ç', 'Ç'), array('n', 'N', 'c', 'C', ), $string);
+	$string = str_replace(array("\\", "¨", "º", "-", "~", "#", "@", "|", "!", "\"", "·", "$", "%", "&", "/", "(", ")", "?", "'", "¡", "¿", "[", "^", "`", "]", "+", "}", "{", "¨", "´", ">", "<", ";", ",", ":", ".", " "), '-', $string);
+	return $string;
+}
+
+echo sanear_string("Javier Gónzalez Rastrojo");
+?>
+<script>
+	pepe = new Array("pepe.php","dbug");
+	function p(pepe){
+		alert(pepe[1]);
+	}
+	p(pepe);
+</script>
