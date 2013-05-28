441c3bcd8e0c4f7b4addc75c9bb77eb918e32e97
darkvier <javi_wallace@hotmail.com>
2013-05-28 11:07:01 +0200

    Registro: estilo centrado

--- registro.php
+++ registro.php
@@ -15,7 +15,7 @@ require('inc/config.php');
 </head>
 <body>
 	<ul id="menudrop">
-		<li><a href="login.php">&lt;-- Login</a></li>
+		<li><a href="login.php">Login</a></li>
 		<li><a href="contacto.php">Contacto</a></li>
 	</ul>
 
@@ -31,24 +31,26 @@ if($_POST){
 	<div class="centrar">
 		<div class="marco" style="width: 300px;">
 			<div id="valida_error"></div>
-			<form name="registro" method='post' action='registro.php'>
-				Nombre: <input type='text' class="validable" size='15' maxlength='20' name='Nombre' value="<?php echo $_POST['Nombre']; ?>" /><br />
-				Apellidos: <input type='text' class="validable" size='25' maxlength='40' name='Apellidos'  value="<?php echo $_POST['Apellidos']; ?>" /><br />
-				Contrase&ntilde;a: <input type='password' class="validable" size='15' name='contrasenia' /><br />
-				Repita la contrase&ntilde;a: <input type='password' class="validable" size='15' name='contrasenia2' /><br />
-				Email: <input type='text' class="validable" size='30' name='Email'  value="<?php echo $_POST['Email']; ?>" /><br />
-
-				Fecha nacimiento: <input type='text' class="validable" size='15' name='nacimiento'  id="datepicker"  value="<?php echo $_POST['nacimiento']; ?>" /><br />
-				
-				Sexo: 
-				<input type="radio" class="validable" name="Sexo" value="H" id="sexo_hombre"/><label for="sexo_hombre" class="label_radio label_Sexo"></label><label for="sexo_hombre">Hombre</label>
-				<input type="radio" class="validable" name="Sexo" value="M" id="sexo_mujer"/><label for="sexo_mujer" class="label_radio label_Sexo"></label><label for="sexo_mujer">Mujer</label><br />
-				
-				<input type="checkbox" class="validable" name="tos" id="checkbox_tos" value="tos_yes"><label for="checkbox_tos">Acepto los terminos de uso</label><br>
-
-				Provincia: <select name="Provincia" class="validable"><?php require("inc/select_provincias.html"); ?></select><br>
-				<input type="hidden" name="Registro" value="yes"/>
-				<button type='button' name='registro' value='Registrarse' onclick="validador('submit')">Registrarse</button>
-			</form>
+			<div style="text-align: center;">
+				<form name="registro" method='post' action='registro.php' style="text-align: left;">
+					Nombre: <input type='text' class="validable" size='26' maxlength='20' name='Nombre' value="<?php echo $_POST['Nombre']; ?>" /><br />
+					Apellidos: <input type='text' class="validable" size='25' maxlength='40' name='Apellidos'  value="<?php echo $_POST['Apellidos']; ?>" /><br />
+					Contrase&ntilde;a: <input type='password' class="validable" size='23' name='contrasenia' /><br />
+					Repita la contrase&ntilde;a: <input type='password' class="validable" size='15' name='contrasenia2' /><br />
+					Email: <input type='text' class="validable" size='29' name='Email'  value="<?php echo $_POST['Email']; ?>" /><br />
+	
+					Fecha nacimiento: <input type='text' class="validable" size='18' name='nacimiento'  id="datepicker"  value="<?php echo $_POST['nacimiento']; ?>" /><br />
+					Provincia: <select name="Provincia" class="validable"><?php require("inc/select_provincias.html"); ?></select><br>
+					
+					Sexo: 
+					<input type="radio" class="validable" name="Sexo" value="H" id="sexo_hombre"/><label for="sexo_hombre" class="label_radio label_Sexo"></label><label for="sexo_hombre">Hombre</label>
+					<input type="radio" class="validable" name="Sexo" value="M" id="sexo_mujer"/><label for="sexo_mujer" class="label_radio label_Sexo"></label><label for="sexo_mujer">Mujer</label><br />
+					
+					<input type="checkbox" class="validable" name="tos" id="checkbox_tos" value="tos_yes"><label for="checkbox_tos">Acepto los <a href="post.php?tos=1" target="_blank">terminos de uso</a></label><br>
+	
+					<input type="hidden" name="Registro" value="yes"/>
+					<button type='button' name='registro' value='Registrarse' onclick="validador('submit')">Registrarse</button>
+				</form>
+			</div>
 		</div>
 	</div>
